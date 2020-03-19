a71fee314e02c803effb82cd594260ea35412780
charuagreya <62243480+charuagreya@users.noreply.github.com>
2020-03-19 17:28:01 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+    @override
   Widget build(BuildContext context) {
+      Widget image_carousel = new Container(
+        height: 200.0,
+        //child: new Caro,
+      );
+
     return Scaffold(
       appBar: new AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.red,
         title: Text('FashApp'),
         actions: <Widget>[
@@ -47,6 +53,7 @@ class _HomePageState extends State<HomePage> {
                   child: Icon(Icons.person, color: Colors.white,),
                 ),
               ),
+
               decoration: new BoxDecoration(
                   color: Colors.pink
               ),
@@ -58,38 +65,54 @@ class _HomePageState extends State<HomePage> {
               onTap: (){},
               child: ListTile(
                  title: Text('Home Page'),
-                 leading: Icon(Icons.person),
+                 leading: Icon(Icons.home, color: Colors.redAccent,),
               ),
             ),
             InkWell(
               onTap: (){},
               child: ListTile(
                 title: Text('My Orders'),
-                leading: Icon(Icons.shopping_basket),
+                leading: Icon(Icons.shopping_basket, color: Colors.redAccent),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
               ),
             ),
             InkWell(
               onTap: (){},
               child: ListTile(
                 title: Text('Categories'),
-                leading: Icon(Icons.dashboard),
+                leading: Icon(Icons.dashboard, color: Colors.redAccent),
               ),
             ),
             InkWell(
               onTap: (){},
               child: ListTile(
-                title: Text('Home Page'),
-                leading: Icon(Icons.home),
+                title: Text('Favourite'),
+                leading: Icon(Icons.favorite, color: Colors.redAccent),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings, color: Colors.blue,),
               ),
             ),
             InkWell(
               onTap: (){},
               child: ListTile(
-                title: Text('Home Page'),
-                leading: Icon(Icons.home),
+                title: Text('about'),
+                leading: Icon(Icons.help,color: Colors.blue,),
               ),
             )
-
 
           ],
         ),
